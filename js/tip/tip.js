612495383c52e6b73e82f124b70f416b28e7c811
vin@wataidesign.com <vin@wataidesign.com>
2020-09-16 11:47:42 +0800

    tip all ok

--- js/tip/tip.js
+++ js/tip/tip.js
@@ -5,12 +5,6 @@ $(function () {
     $('.okb').on('click', function () {
         let parent = $(this).parent();
         $(parent.next()).toggleClass('none');
-        // if (parent.attr('class') === '4') {
-        //     $('section').addClass('none');
-        //     $('section.1').removeClass('none');
-        // } else {
-        //     return
-        // }
     });
 
     $('.nxb,.mnxb').on('click', function () {
